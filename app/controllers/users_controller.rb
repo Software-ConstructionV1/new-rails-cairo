92908cf755b98cde8e7721e00b0b918777fa06fb
A7med-zaki <ahmedzaki123789@gmail.com>
2025-05-17 15:30:04 +0300

    feat: implement user and post models with validations and styling
    Add User and Post models with necessary validations and migrations.
    Update user views to reflect changes in date of birth field naming.
    Implement CRUD operations in UsersController.
    Add CSS styling for user forms and improve UI consistency.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,51 @@
 class UsersController < ApplicationController
-  def index 
-   # logic
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new({
+      name: params[:user][:name],
+      DOB: params[:user][:DOB],
+      email: params[:user][:email],
+      phone_number: params[:user][:phone_number]
+    })
+    if @user.save
+      redirect_to @user, notice: 'User was created successfully.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update({
+      name: params[:user][:name],
+      DOB: params[:user][:DOB],
+      email: params[:user][:email],
+      phone_number: params[:user][:phone_number]
+    })
+      redirect_to @user, notice: 'User was updated successfully.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: 'User was deleted successfully.'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :name, :DOB, :email, :phone_number, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :email, uniqueness: true
 end
