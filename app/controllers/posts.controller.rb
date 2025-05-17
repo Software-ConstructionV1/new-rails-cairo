217b0258dab7f8e697fe33ddde9a377048f59586
Esraa Khalifa <esraakhalifa5@gmail.com>
2025-05-17 13:43:30 +0300

    feat: add posts controller with full CRUD operations

---
+++ app/controllers/posts.controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+
+    def index 
+        @posts = Post.all
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def new
+        @post = Post.new
+    end
+    
+    def create
+
+        @post = Post.new(params[:title, :content])
+        if @post.save
+            redirect_to @post, notice: "Post was successfully created."
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    def edit
+        @post = Post.find(params[:id])
+    end
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(params[:title, :content])
+            redirect_to @post, notice: "Post was successfully updated."
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path, notice: "Post was successfully deleted."
+    end
+
+    # private
+
+    # def post_params
+    #     params.require(:post).permit(:title, :content)
+    # end
+end
