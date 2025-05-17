05bc2fbf100530d8a0e6bd867bb36502dced6cec
NadaEmamm <nadaemam.me@gmail.com>
2025-05-17 14:22:35 +0300

    made validates for post and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\z/
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: "is not a valid email" }
+    validates :dob, presence: true
+    validates :phone_number, presence: true, uniqueness: true
+
 end
