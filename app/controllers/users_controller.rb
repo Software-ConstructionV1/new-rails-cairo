30d57b858cd5fbbf4961ca0ad8461cad390a5df4
NadaEmamm <nadaemam.me@gmail.com>
2025-05-17 14:13:19 +0300

    made CRUD operations for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,41 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def show 
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+  def create 
+
+    @user = User.new(name: params[:user][:name], email: params[:user][:email], phone_number: params[:user][:phone_number], dob: params[:user][:dob])
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update  
+    @user = User.find(params[:id])
+    if @user.update(name: params[:user][:name], email: params[:user][:email],phone_number: params[:user][:phone_number], dob: params[:user][:dob])
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url, notice: 'User was successfully destroyed.'
   end
 end
