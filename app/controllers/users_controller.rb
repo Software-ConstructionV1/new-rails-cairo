ecdee0e0c3954988411e9dcd4cefbcedb8f132f8
Ahmed-Sayed-1 <ahmed2853519521@gmail.com>
2025-05-17 15:15:10 +0300

    Added user CRUD operation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,35 @@
 class UsersController < ApplicationController
   def index
-    
+    @users= User.all
+  end
+  def show
+    @user = User.find(params[:id])
+  end
+  def new
+    @user=User.new
+  end
+  def create
+    @user=User.new(name: params[:user][:name], email: params[:user][:email], date_of_birth: params[:user][:date_of_birth], phone_number: params[:user][:phone_number])
+    if @user.save
+      redirect_to @user, notice: "User Created!"
+    else
+        render :new, status: 422
+    end
+  end
+  def edit
+    @user= User.find(params[:id])
+  end
+  def update
+    @user= User.find(params[:id])
+    if @user.update(name: params[:user][:name], email: params[:user][:email], date_of_birth: params[:user][:date_of_birth], phone_number: params[:user][:phone_number])
+      redirect_to @user, notice: "User updated"
+    else
+        render :new, status: 422
+    end
+  end
+  def destroy
+    @user= User.find(params[:id])
+    @user.destroy!
+    redirect_to @user
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,1 +1,2 @@
-
+class POST < ApplicationRecord
+end
