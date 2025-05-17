5cdbaad49c7f3419a789809330cc841435f54bd5
Arwa Mohammed <arwamohmd.32@gmail.com>
2025-05-17 15:21:16 +0300

    Add users resource and implement user model with validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_17_110842) do
-  create_table "users_tables", force: :cascade do |t|
+ActiveRecord::Schema[8.0].define(version: 2025_05_17_121420) do
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.date "dob"
     t.string "email"
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
