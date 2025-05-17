9a62f041a2fee349c2da52b41eccb55b01fcc88f
Youssef Medhat <youssefmedhatt2001@gmail.com>
2025-05-17 13:49:50 +0300

    feat: implement UsersController with CRUD actions for user management

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  def show
+    @user = User.find(params[:id])
+  end
+  def new
+    @user = User.new
+  end
+  def create
+    puts params[:user]
+    @user = User.new(name: params[:user][:name], dob: params[:user][:dob], email: params[:user][:email], phone_number: params[:user][:phone_number],address: '21 elFardous st.')
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(name: params[:user][:name], dob: params[:user][:dob], email: params[:user][:email], phone_number: params[:user][:phone_number])
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to @user, notice: 'User was successfully destroyed.'
+  end
+end
