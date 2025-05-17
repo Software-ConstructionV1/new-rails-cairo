841adda123d7b59f415701545ec62e6d2057b5fc
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-05-17 18:34:26 +0300

    Add address field to User model with validation and update database schema. Implement migration to add address column with default value.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_17_114126) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_17_152835) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_17_114126) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address", default: "Address not provided", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
