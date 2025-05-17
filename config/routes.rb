c69eb2694548b79d2cb6e4bcfb6b677183871a85
Abdullah <bmaher.ca@gmail.com>
2025-05-17 11:12:29 +0000

    feat(routes): define root path, posts resources, and users index route

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,11 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "posts#index"
+
+  resources :posts
+
+  get "/", to: "posts#index"
+
+  get "/users" => "users#index"
 end
