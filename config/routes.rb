de30f8e70558ea7d2b017d8f3dc4d39267a27247
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-05-17 14:28:35 +0300

    Add PostsController, Post model, and associated views for managing posts, including create, read, update, and delete functionality. Implement form changes for content input and set up database migration for posts table.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "posts#index"
+  
+  resources :posts
   get "users" , to: "users#index"
 end
