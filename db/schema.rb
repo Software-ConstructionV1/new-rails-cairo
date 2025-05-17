5e370406f60e3f5af8a3cb32717764915dcb6ce4
Esraa Khalifa <esraakhalifa5@gmail.com>
2025-05-17 14:47:55 +0300

    feat: add users migration and the user model, with the presence validations, and email uniqueness and format validation, and the email index in the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_17_103457) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_17_111203) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.date "dob", null: false
+    t.string "email", null: false
+    t.string "phone_number", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
