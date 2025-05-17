f736cf1efa99cfe6cdec9201b4ed8a8308bb841b
JanaHazem12 <jana.hazem.abbass@gmail.com>
2025-05-17 14:38:51 +0300

    Generate/Migrate 'User' Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_17_103549) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_17_113616) do
   create_table "posts_tables", force: :cascade do |t|
     t.string "title"
     t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.date "DOB"
+    t.string "email"
+    t.integer "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
